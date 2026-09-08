3e69e4d38e6cea67f5fba7f1332a36c9fadc1161
devCarlJoseph <carljosephsumagang17@gmail.com>
2026-09-09 01:32:23 +0800

    feat(home): add home category section

---
+++ src/components/features/users/home/popular-store-section.tsx
@@ -0,0 +1,7 @@
+export function PopularStoreSection() {
+  return (
+    <section>
+      <div>This is My Shop by Category section</div>
+    </section>
+  );
+}
--- src/pages/user/home/index.tsx
+++ src/pages/user/home/index.tsx
@@ -1,19 +1,15 @@
-import { CategorySection } from "@/components/features/users/home/categories-section";
 import { HomeHeroSection } from "@/components/features/users/home/hero-section";
+import { HomeCategoriesSection } from "@/components/features/users/home/home-categories-section";
 import { PopularStoreSection } from "@/components/features/users/home/popular-stores-section";
 import { PromoBannerSection } from "@/components/features/users/home/promo-banner-section";
 import { StoreSection } from "@/components/features/users/home/stores-section";
-import { WhyChooseUsSection } from "@/components/features/users/home/why-choose-us-section";
 
 export default function HomePage() {
   return (
     <>
       <HomeHeroSection />
-      <StoreSection />
-      <PopularStoreSection />
-      <CategorySection />
+      <HomeCategoriesSection />
       <PromoBannerSection />
-      <WhyChooseUsSection />
     </>
   );
 }
